101c459ecbf1fc296839be6b6524ce293f04c953
rushika-addweb <68734457+rushika-addweb@users.noreply.github.com>
2020-08-14 16:12:06 +0530

    React js
    Reactjs Assignment

---
+++ App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import Menu from './Menu';
+import About from './About';
+import Contact from './Contact';
+import{BrowserRouter,Route,Switch} from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div>
+     <Menu/>
+      <Switch>
+     <Route path="/" exact component={Home}/>
+     <Route path="/about" component={About}/>
+     <Route path="/contact" component={Contact}/>
+     </Switch>
+    </div>
+    </BrowserRouter>
+  );
+}
+
+const Home = () =>{
+
+  return(
+    <div className="Contactstyle">
+    <h2> Welcome to Home Page. </h2>
+    </div>
+
+    )
+}
+
+export default App;
---
+++ Menu.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Menustyle.css';
+import {Link} from 'react-router-dom';
+
+const Menu = () =>{
+	return(
+		<div className="Menustyle">
+			<ul>
+			<li><Link to="/">Home</Link></li>
+			<li><Link to="Contact">Contact</Link></li>
+			<li><Link to="About">About</Link></li>
+			</ul>
+		</div>
+		)
+} 
+
+
+
+
+export default Menu;
